bf0c95b08efc6034de189e8a638c2d6f20baceef
Soveer P Shah <soveer18@gmail.com>
2026-02-06 18:17:02 +0530

    changed css and cookie issues

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -47,38 +47,35 @@ function Homepage({setisAuthenticated}) {
 
   return (
     <>
-     <div className="card w-96 bg-white/10 backdrop-blur-md border border-white/20 shadow-2xl p-6 text-center rounded-2xl text-white">
-
-  {user.photo && (
-      <img
-        src={user.photo}
-        alt="Profile"
-        className="w-20 h-20 rounded-full mx-auto mb-4 object-cover border-2 border-white/30"
-      />
-    )}
-
-    <h2 className="text-3xl font-semibold mb-3">Welcome!</h2>
-
-    <p className="text-lg mb-4 text-gray-200">
-      You have successfully logged in as
-    </p>
-
-    <p className="text-xl font-medium mb-4">
-      {user.firstName} {user.lastName}
-    </p>
-
-    <p className="text-sm text-gray-300 mb-6">
-      {user.emailId}
-    </p>
-
-    <button
-      onClick={handleLogout}
-      className="w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600 transition"
+     <div className="min-h-screen w-full bg-gradient-to-b from-[#324d8b] via-[#040b29] to-black">
+      <div
+      className="min-h-screen flex items-center justify-center p-4"
+      style={{ fontFamily: "sans-serif" }}
     >
-      Logout
-    </button>
-</div>
+      <div className="card w-96 bg-cyan-100 shadow-xl p-6 text-center text-gray-900">
 
+      {user.photo && (
+        <img
+          src={user.photo}
+          alt="Profile"
+          className="w-20 h-20 rounded-full mx-auto mb-4 object-cover"
+        />
+      )}
+
+        <h2 className="card-title justify-center text-3xl mb-4">Welcome!</h2>
+        <p className="text-lg mb-6 ">
+          You have successfully logged in as <strong>{user.firstName} {user.lastName}</strong>.
+        </p>
+        <p>Enjoy your session!</p>
+        <p>Your email is: <strong>{user.emailId}</strong></p>
+
+        <button onClick={handleLogout} 
+        className="w-full bg-blue-400 text-white py-2 mt-5 rounded-md hover:bg-red-400 transition">
+          Logout
+        </button>
+      </div>
+    </div>
+    </div>
     </>
   );
 }
